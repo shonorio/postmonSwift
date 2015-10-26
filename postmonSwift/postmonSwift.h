7cec82dc42226e7cc682fbdbca5f48903e6613d8
Solli Honorio <shonorio@gmail.com>
2015-10-26 11:39:34 -0200

    Iniciando o código do postmonSwift

---
+++ postmonSwift/postmonSwift.h
@@ -0,0 +1,19 @@
+//
+//  postmonSwift.h
+//  postmonSwift
+//
+//  Created by Solli Honorio on 10/26/15.
+//  Copyright © 2015 Solli Honorio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for postmonSwift.
+FOUNDATION_EXPORT double postmonSwiftVersionNumber;
+
+//! Project version string for postmonSwift.
+FOUNDATION_EXPORT const unsigned char postmonSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <postmonSwift/PublicHeader.h>
+
+
